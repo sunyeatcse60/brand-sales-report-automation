5ee7b9fd91a5ee079b233289f742dfedf88b4830
sunyeatcse60 <sunyeat.cse60@gmail.com>
2026-07-04 17:19:17 +0600

    update some part

--- upload_to_sheet.py
+++ upload_to_sheet.py
@@ -2,6 +2,8 @@ import gspread
 import pandas as pd
 from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -31,16 +33,46 @@ def upload_excel_to_sheet(excel_file, sheet_name):
             cols=50
         )
 
+    # Read Excel
     df = pd.read_excel(excel_file)
 
+    # Clear previous data
     worksheet.clear()
 
+    # Upload dataframe starting from A5
     set_with_dataframe(
         worksheet,
         df,
+        row=5,
+        col=1,
         include_index=False,
         include_column_header=True,
         resize=True
+    )
+
+    # Current Date (Bangladesh Time)
+    today = datetime.now(
+        ZoneInfo("Asia/Dhaka")
+    ).strftime("%b-%y")        # Example: Jul-26
+
+    # Date in middle
+    worksheet.update("J2", [[today]])
+
+    # Format the date cell
+    worksheet.format(
+        "J2",
+        {
+            "backgroundColor": {
+                "red": 0.85,
+                "green": 0.95,
+                "blue": 0.85
+            },
+            "textFormat": {
+                "bold": True,
+                "fontSize": 14
+            },
+            "horizontalAlignment": "CENTER"
+        }
     )
 
     print(f"{sheet_name} Updated Successfully")
